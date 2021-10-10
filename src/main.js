11134a17e725f3b35f3eac097f66cbe641354bad
Paulo <paulo.cardoso@jumpseller.com>
2021-10-10 22:34:05 +0100

    workflow

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 import vueCountryRegionSelect from 'vue-country-region-select'
 
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 Vue.use(vueCountryRegionSelect)
 
 Vue.config.productionTip = false
