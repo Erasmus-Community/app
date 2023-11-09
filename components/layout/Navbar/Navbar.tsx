2a5dfbe5b2b829384310632e5ba1a5cf6bd41cf2
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-11-09 18:23:01 +0000

    adding stuff and removing node_modules

--- components/layout/Navbar/Navbar.tsx
+++ components/layout/Navbar/Navbar.tsx
@@ -1,5 +1,16 @@
-/* TODO: change this */
+"use client";
+
+import classNames from "classnames";
+import { useState } from "react";
+
+/*
+ * missing config and links from the information
+ */
+
 const Navbar = () => {
+  const [mobileNavbarOpen, setMobileNavbarOpen] = useState<boolean>(false);
+  const [profileDropdownOpen, setProfileDropdownOpen] =
+    useState<boolean>(false);
   return (
     <>
       <nav className="bg-gray-800">
@@ -12,14 +23,12 @@ const Navbar = () => {
                 className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
+                onClick={() =>
+                  setMobileNavbarOpen((currentStatus) => !currentStatus)
+                }
               >
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
-                {/* <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          --> */}
                 <svg
                   className="block h-6 w-6"
                   fill="none"
@@ -34,11 +43,6 @@ const Navbar = () => {
                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                   />
                 </svg>
-                {/* <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          --> */}
                 <svg
                   className="hidden h-6 w-6"
                   fill="none"
@@ -65,7 +69,6 @@ const Navbar = () => {
               </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
-                  {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                   <a
                     href="#"
                     className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
@@ -117,7 +120,6 @@ const Navbar = () => {
                 </svg>
               </button>
 
-              {/* <!-- Profile dropdown --> */}
               <div className="relative ml-3">
                 <div>
                   <button
@@ -126,6 +128,9 @@ const Navbar = () => {
                     id="user-menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
+                    onClick={() =>
+                      setProfileDropdownOpen((currentStatus) => !currentStatus)
+                    }
                   >
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">Open user menu</span>
@@ -136,25 +141,18 @@ const Navbar = () => {
                     />
                   </button>
                 </div>
-                {/* 
-          <!--
-            Dropdown menu, show/hide based on menu state.
 
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          --> */}
                 <div
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className={classNames({
+                    hidden: !profileDropdownOpen,
+                    "absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none":
+                      profileDropdownOpen,
+                  })}
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="user-menu-button"
                   tabIndex={-1}
                 >
-                  {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
                   <a
                     href="#"
                     className="block px-4 py-2 text-sm text-gray-700"
@@ -188,10 +186,14 @@ const Navbar = () => {
           </div>
         </div>
 
-        {/* <!-- Mobile menu, show/hide based on menu state. -->*/}
-        <div className="sm:hidden" id="mobile-menu">
+        <div
+          className={classNames({
+            hidden: !mobileNavbarOpen,
+            "sm:hidden": mobileNavbarOpen,
+          })}
+          id="mobile-menu"
+        >
           <div className="space-y-1 px-2 pb-3 pt-2">
-            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
             <a
               href="#"
               className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
--- supabase/migrations/20231109164944_initTables.sql
+++ supabase/migrations/20231109164944_initTables.sql
@@ -2,7 +2,10 @@ CREATE TABLE profiles (
     id uuid default uuid_generate_v4(),
     first_name text,
     last_name text,
+    birth_date date,
 
+    created_at timestamptz DEFAULT NOW(),
+    updated_at timestamptz DEFAULT NOW(),
     primary key (id)
 );
 
@@ -11,7 +14,11 @@ CREATE TABLE profiles (
 CREATE TABLE organizations (
     id uuid default uuid_generate_v4(),
     name text,
+    country text,
+    logo text,
 
+    created_at timestamptz DEFAULT NOW(),
+    updated_at timestamptz DEFAULT NOW(),
     primary key (id)
 );
 
@@ -19,7 +26,20 @@ CREATE TABLE projects (
     id uuid default uuid_generate_v4(),
     title text,
     description text,
+    hosted_by uuid,
+    partner_orgs uuid[],    
 
-    primary key (id)
+    created_at timestamptz DEFAULT NOW(),
+    updated_at timestamptz DEFAULT NOW(),
+    primary key (id),
+    foreign key (hosted_by) REFERENCES organizations(id)
 );
 
+/* missing this -- Enable MODDATETIME extension
+create extension if not exists moddatetime schema extensions;
+
+-- This will set the `updated_at` column on every update
+create trigger handle_updated_at before update on YOUR_TABLE_NAME
+  for each row execute procedure moddatetime (updated_at);
+
+*/
